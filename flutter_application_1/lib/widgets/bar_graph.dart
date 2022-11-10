03db993e89abee9dac9888ed092a0215fb5ca51b
LeoMerc <leonardoomercado2@gmail.com>
2022-11-10 09:08:55 -0800

    finished bar graph:

--- flutter_application_1/lib/widgets/bar_graph.dart
+++ flutter_application_1/lib/widgets/bar_graph.dart
@@ -6,34 +6,111 @@ class barGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    late List<BarChartGroupData> rawBarGroups;
+
+    final barGroup1 = makeGroupData(0, 418, Color.fromARGB(255, 65, 189, 247));
+    final barGroup2 = makeGroupData(1, 228, Color.fromARGB(255, 14, 222, 202));
+    final barGroup3 = makeGroupData(2, 117, Colors.lightGreenAccent);
+    final barGroup4 = makeGroupData(3, 67, Colors.yellow);
+    final barGroup5 = makeGroupData(4, 39, Colors.deepOrange);
+    final barGroup6 = makeGroupData(5, 35, Colors.pink);
+    final barGroup7 = makeGroupData(6, 33, Color.fromARGB(255, 65, 189, 247));
+    final barGroup8 = makeGroupData(7, 8, Color.fromARGB(255, 14, 222, 202));
+    final barGroup9 = makeGroupData(8, 7, Colors.lightGreenAccent);
+    final barGroup10 = makeGroupData(9, 3, Colors.yellow);
+    final barGroup11 = makeGroupData(10, 3, Colors.deepOrange);
+    final barGroup12 = makeGroupData(11, 2, Colors.pink);
+    final barGroup13 = makeGroupData(12, 2, Color.fromARGB(255, 65, 189, 247));
+    final barGroup14 = makeGroupData(13, 1, Color.fromARGB(255, 14, 222, 202));
+
+    final items = [
+      barGroup1,
+      barGroup2,
+      barGroup3,
+      barGroup4,
+      barGroup5,
+      barGroup6,
+      barGroup7,
+      barGroup8,
+      barGroup9,
+      barGroup10,
+      barGroup11,
+      barGroup12,
+      barGroup13,
+      barGroup14,
+    ];
+
+    rawBarGroups = items;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 18.0),
       child: Container(
         height: 400,
         width: double.infinity / 1,
         child: BarChart(
-          BarChartData(barGroups: [
-            BarChartGroupData(x: 0, barRods: [
-              BarChartRodData(toY: 418, color: Colors.lightBlueAccent),
-              BarChartRodData(toY: 228, color: Color.fromARGB(255, 14, 222, 202)),
-              BarChartRodData(toY: 117, color: Colors.lightGreenAccent),
-              BarChartRodData(toY: 67, color: Colors.yellow),
-              BarChartRodData(toY: 39, color: Colors.deepOrange),
-              BarChartRodData(toY: 35, color: Colors.pink),
-              BarChartRodData(toY: 33, color: Colors.lightBlueAccent),
-              BarChartRodData(toY: 8, color: Color.fromARGB(255, 14, 222, 202)),
-              BarChartRodData(toY: 7, color: Colors.lightGreenAccent),
-              BarChartRodData(toY: 3, color: Colors.yellow),
-              BarChartRodData(toY: 3, color: Colors.deepOrange),
-              BarChartRodData(toY: 2, color: Colors.pink),
-              BarChartRodData(toY: 2, color: Colors.lightBlueAccent),
-              BarChartRodData(toY: 1, color: Color.fromARGB(255, 14, 222, 202)),
-            ])
-          ]),
-          swapAnimationDuration: Duration(milliseconds: 150), // Optional
+          BarChartData(
+              titlesData: FlTitlesData(
+                show: true,
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: bottomTitles,
+                    reservedSize: 42,
+                  ),
+                ),
+              ),
+              barGroups: rawBarGroups,
+              ),
+          swapAnimationDuration: const Duration(milliseconds: 150), // Optional
           swapAnimationCurve: Curves.linear, // Optional
         ),
       ),
+    );
+  }
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    final titles = <String>[
+      'Neighbor',
+      'Google Search',
+      'N/F',
+      'Website',
+      'Facebook',
+      'Yard Sign',
+      'Drive By',
+      'Email',
+      'Billboard',
+      'Twitter',
+      'Post Card',
+      'Instagram',
+      'Church Offer',
+      ' Youtube'
+    ];
+
+    final Widget text = Text(
+      titles[value.toInt()],
+      style: const TextStyle(
+        color: Color(0xff7589a2),
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+    );
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16, //margin top
+      child: text,
+    );
+  }
+
+  BarChartGroupData makeGroupData(int x, double y1, Color color) {
+    return BarChartGroupData(
+      barsSpace: 9,
+      x: x,
+      barRods: [
+        BarChartRodData(
+          toY: y1,
+          color: color,
+        ),
+      ],
     );
   }
 }
